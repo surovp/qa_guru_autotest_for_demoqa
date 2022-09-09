9029729edc192680df1731b73b436020879f3b34
Pavel Surov <surov_p@tanukitech.ru>
2022-09-09 21:23:33 +0500

    add function test_form

--- test_practice_form.py
+++ test_practice_form.py
@@ -1,6 +1,5 @@
-# Разработай автотест на заполнение и отправку формы https://demoqa.com/automation-practice-form
 from selene.support.shared import browser
-from selene import be,have
+from selene import be,have,by
 import pytest
 
 @pytest.fixture()
@@ -10,8 +9,24 @@ def browser_configuration():
 
 @pytest.fixture()
 def open_browser_and_website(browser_configuration):
+    browser.config.browser_name = 'chrome'
     browser.open_url("https://demoqa.com/automation-practice-form")
 
 
 def test_form(open_browser_and_website):
-    pass
+    browser.element("#firstName").type("Ivan")
+    browser.element("#lastName").type("Ivanov")
+    browser.element("#userEmail").type("ivan123@test.com")
+    browser.element("#gender-radio-1").double_click()
+    browser.element("#userNumber").type("75550001100")
+
+    browser.element("#dateOfBirthInput").click()
+    browser.element("#dateOfBirthInput").clear().type("01 Jan 1997")
+    # subject
+    browser.element("#hobbies-checkbox-3").double_click()
+    # File
+    browser.element("#currentAddress").type("Russia,Moscow")
+    #browser.element("#state").click()
+    # state
+    # city
+    browser.element("#submit").click()
