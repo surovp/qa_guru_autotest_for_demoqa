2275899bc64b85efcae4db7882a68a43008dc160
Pavel Surov <surov_p@tanukitech.ru>
2022-09-09 19:53:15 +0500

    add 2 fixtures

--- test_practice_form.py
+++ test_practice_form.py
@@ -1,5 +1,17 @@
 # Разработай автотест на заполнение и отправку формы https://demoqa.com/automation-practice-form
+from selene.support.shared import browser
+from selene import be,have
+import pytest
+
+@pytest.fixture()
+def browser_configuration():
+    browser.config.window_width = 1400
+    browser.config.window_height = 1600
+
+@pytest.fixture()
+def open_browser_and_website(browser_configuration):
+    browser.open_url("https://demoqa.com/automation-practice-form")
 
 
-def test_form():
+def test_form(open_browser_and_website):
     pass
