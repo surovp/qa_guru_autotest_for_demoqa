10553b7fb455502c9398e362aa939eab4c1d5338
Pavel Surov <surov_p@tanukitech.ru>
2022-09-12 11:46:14 +0500

    готовый автотест

--- test_practice_form.py
+++ test_practice_form.py
@@ -1,32 +1,61 @@
+import os
+from selene import have
 from selene.support.shared import browser
-from selene import be,have,by
 import pytest
 
 @pytest.fixture()
-def browser_configuration():
+def browser_configs():
     browser.config.window_width = 1400
     browser.config.window_height = 1600
+    browser.config.browser_name = 'chrome'
 
 @pytest.fixture()
-def open_browser_and_website(browser_configuration):
-    browser.config.browser_name = 'chrome'
+def open_and_close_form(browser_configs):
     browser.open_url("https://demoqa.com/automation-practice-form")
+    yield
+    browser.element("#closeLargeModal").double_click()
+    browser.close()
 
-
-def test_form(open_browser_and_website):
+def test_form(open_and_close_form):
+    # Name
     browser.element("#firstName").type("Ivan")
     browser.element("#lastName").type("Ivanov")
+    # Email
     browser.element("#userEmail").type("ivan123@test.com")
+    # Gender
     browser.element("#gender-radio-1").double_click()
-    browser.element("#userNumber").type("75550001100")
-
+    # Mobile
+    browser.element("#userNumber").type("5550001100")
+    # Date of Birth
     browser.element("#dateOfBirthInput").click()
-    browser.element("#dateOfBirthInput").clear().type("01 Jan 1997")
-    # subject
-    browser.element("#hobbies-checkbox-3").double_click()
-    # File
+    browser.element(".react-datepicker__month-select").type("January")
+    browser.element(".react-datepicker__year-select").type("1997")
+    browser.element("[aria-label='Choose Saturday, January 25th, 1997']").click()
+    # Subjects
+    browser.element("#subjectsInput").type("English").press_enter()
+    # Hobbies
+    browser.element("[for=hobbies-checkbox-3]").click()
+    # Picture
+    browser.element("#uploadPicture").send_keys(os.path.abspath("Test_files/file1.png"))
+    # Current Address
     browser.element("#currentAddress").type("Russia,Moscow")
-    #browser.element("#state").click()
-    # state
-    # city
-    browser.element("#submit").click()
+    # State and City
+    browser.element("#react-select-3-input").type("Haryana").press_enter()
+    browser.element("#react-select-4-input").type("Karnal").press_enter()
+    # Button Submit
+    browser.element('#submit').press_enter()
+
+    # check form
+    browser.element('#example-modal-sizes-title-lg').should(have.text('Thanks for submitting the form'))
+    browser.element(".table-responsive").should(have.text("Ivan"))
+    browser.element(".table-responsive").should(have.text("Ivanov"))
+    browser.element(".table-responsive").should(have.text("ivan123@test.com"))
+    browser.element(".table-responsive").should(have.text("Male"))
+    browser.element(".table-responsive").should(have.text("5550001100"))
+    browser.element(".table-responsive").should(have.text("25 January,1997"))
+    browser.element(".table-responsive").should(have.text("English"))
+    browser.element(".table-responsive").should(have.text("Music"))
+    browser.element(".table-responsive").should(have.text("file1.png"))
+    browser.element(".table-responsive").should(have.text("Russia,Moscow"))
+    browser.element(".table-responsive").should(have.text("Haryana"))
+    browser.element(".table-responsive").should(have.text("Karnal"))
